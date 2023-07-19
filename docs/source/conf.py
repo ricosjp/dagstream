774f127244a8f6ff56e3bf1f14fe39041c0d25c0
sakamoto <sakamoto@ricos.co.jp>
2023-07-19 15:04:46 +0900

    [add] add sphinx mermaid

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+import sphinx_rtd_theme
+
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../src"))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,15 +22,25 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'sphinx.ext.napoleon',  # google, numpy styleのdocstring対応
+    'sphinxcontrib.mermaid',  # To write diagram
+    "sphinxcontrib.jquery"
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    'display_version': True
+}
