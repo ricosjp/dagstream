2cfcf4877fedf99f841ea270e1742f4db5075f4d
sakamoto <sakamoto@ricos.co.jp>
2023-08-01 13:19:18 +0900

    [fix] fix exectuor

--- src/dagstream/executor.py
+++ src/dagstream/executor.py
@@ -3,12 +3,12 @@ from dagstream.graph_components import FunctionalDag
 
 
 class StreamExecutor:
-    def __init__(self) -> None:
-        ...
+    def __init__(self, functional_dag: FunctionalDag) -> None:
+        self._dag = functional_dag
 
-    def run(sel, functional_dag: FunctionalDag) -> None:
-        while functional_dag.is_active:
-            nodes = functional_dag.get_ready()
+    def run(self, *args, **kwargs) -> None:
+        while self._dag.is_active:
+            nodes = self._dag.get_ready()
             for node in nodes:
                 node.run()
-                functional_dag.done(node)
+                self._dag.done(node)
